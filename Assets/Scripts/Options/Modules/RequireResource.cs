e319eaaadfa46a41490b7c6d7069882be0580dfc
o0Ezequiel0o <ezematteazzi@gmail.com>
2026-04-21 19:49:49 -0300

    Window displaying fix

--- Assets/Scripts/Options/Modules/RequireResource.cs
+++ Assets/Scripts/Options/Modules/RequireResource.cs
@@ -9,7 +9,7 @@ public class RequireResource : OptionModule
 
     public override bool CanExecute()
     {
-        int amount = ResourceManager.Resources.GetResourceAmount(resource);;
+        int amount = ResourceManager.Resources.GetResourceAmount(resource);
         return amount >= required;
     }
 
