41956d313e94f1d400af6d24339727e142f8ef38
o0Ezequiel0o <ezematteazzi@gmail.com>
2026-06-02 15:25:11 -0300

    cost and rarity for theology tree

---
+++ Assets/Scripts/Unlocks/UnlockRarity.cs
@@ -0,0 +1,6 @@
+public enum Rarity
+{
+    Common,
+    Rare,
+    Legendary
+}
--- Assets/Scripts/Unlocks/Unlocked.cs
+++ Assets/Scripts/Unlocks/Unlocked.cs
@@ -6,16 +6,28 @@ public class Unlocked : ScriptableObject
 {
     [SerializeField] private List<ScriptableObject> unlocked;
 
+    private readonly Dictionary<Rarity, List<ScriptableObject>> filtered = new Dictionary<Rarity, List<ScriptableObject>>();
+
     public List<ScriptableObject> GetCopy() => new List<ScriptableObject>(unlocked);
+    public List<ScriptableObject> GetRarity(Rarity rarity) => filtered[rarity];
 
     public bool Contains(ScriptableObject scriptable)
     {
         return unlocked.Contains(scriptable);
     }
 
-    public void Add(ScriptableObject scriptable)
+    public void Add(ScriptableObject scriptable, Rarity rarity)
     {
         unlocked.Add(scriptable);
+        
+        if (filtered.TryGetValue(rarity, out List<ScriptableObject> filteredList))
+        {
+            filteredList.Add(scriptable);
+        }
+        else
+        {
+            filtered.Add(rarity, new List<ScriptableObject>() { scriptable });
+        }
     }
 
     public void Clear()
